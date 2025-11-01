c4cb1b6f9a008393070300d267ed09a28b11fda0
hazmalam11-ai <hazmalam11@gmail.com>
2025-11-02 01:56:07 +0200

    Delete src/components/src/components directory

--- src/components/src/components/LoadingAnimation.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function LoadingAnimation() {
-  return (
-    <div className="flex items-center justify-center p-4 text-gray-500">
-      Loading...
-    </div>
-  );
-}
