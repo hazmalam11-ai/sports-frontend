5eba34fe5f4b6ecf3bbaeea08a91f0e94f80a2e3
hazmalam11-ai <hazmalam11@gmail.com>
2025-11-02 01:53:30 +0200

    Create LoadingAnimation.tsx
    add: LoadingAnimation placeholder component

---
+++ src/components/src/components/LoadingAnimation.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function LoadingAnimation() {
+  return (
+    <div className="flex items-center justify-center p-4 text-gray-500">
+      Loading...
+    </div>
+  );
+}
